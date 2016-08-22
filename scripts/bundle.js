db5896d82a2dc4725b132036b905346a1bd0883a
Khoa Nguyen <hi@khoanguyen.me>
2016-08-22 22:42:16 +0700

    Rename script to `start` from `run`

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,10 +1,5 @@
-/**
- * React App SDK (https://github.com/kriasoft/react-app)
- *
- * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
+/*
+ * Bundle all front-end file too app/dist with Webpack
  */
 
 const webpack = require('webpack');
